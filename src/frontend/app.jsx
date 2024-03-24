7a2cd4d06fec7f6b13d57e4fb211fafc31759c07
Lukehsu <lukehsu1999@gmail.com>
2024-03-24 12:12:43 -0400

    add video recorder

--- src/frontend/app.jsx
+++ src/frontend/app.jsx
@@ -567,6 +567,139 @@ async function* fetchGeneration(
   reader.releaseLock();
 }
 
+const mimeType = 'video/webm; codecs="opus,vp8"';
+
+const VideoRecorder = () => {
+	const [permission, setPermission] = useState(false);
+
+	const mediaRecorder = useRef(null);
+
+	const liveVideoFeed = useRef(null);
+
+	const [recordingStatus, setRecordingStatus] = useState("inactive");
+
+	const [stream, setStream] = useState(null);
+
+	const [recordedVideo, setRecordedVideo] = useState(null);
+
+	const [videoChunks, setVideoChunks] = useState([]);
+
+	const getCameraPermission = async () => {
+		setRecordedVideo(null);
+		//get video and audio permissions and then stream the result media stream to the videoSrc variable
+		if ("MediaRecorder" in window) {
+			try {
+				const videoConstraints = {
+					audio: false,
+					video: true,
+				};
+				const audioConstraints = { audio: true };
+
+				// create audio and video streams separately
+				const audioStream = await navigator.mediaDevices.getUserMedia(
+					audioConstraints
+				);
+				const videoStream = await navigator.mediaDevices.getUserMedia(
+					videoConstraints
+				);
+
+				setPermission(true);
+
+				//combine both audio and video streams
+
+				const combinedStream = new MediaStream([
+					...videoStream.getVideoTracks(),
+					...audioStream.getAudioTracks(),
+				]);
+
+				setStream(combinedStream);
+
+				//set videostream to live feed player
+				liveVideoFeed.current.srcObject = videoStream;
+			} catch (err) {
+				alert(err.message);
+			}
+		} else {
+			alert("The MediaRecorder API is not supported in your browser.");
+		}
+	};
+
+	const startRecording = async () => {
+		setRecordingStatus("recording");
+
+		const media = new MediaRecorder(stream, { mimeType });
+
+		mediaRecorder.current = media;
+
+		mediaRecorder.current.start();
+
+		let localVideoChunks = [];
+
+		mediaRecorder.current.ondataavailable = (event) => {
+			if (typeof event.data === "undefined") return;
+			if (event.data.size === 0) return;
+			localVideoChunks.push(event.data);
+		};
+
+		setVideoChunks(localVideoChunks);
+	};
+
+	const stopRecording = () => {
+		setPermission(false);
+		setRecordingStatus("inactive");
+		mediaRecorder.current.stop();
+
+		mediaRecorder.current.onstop = () => {
+			const videoBlob = new Blob(videoChunks, { type: mimeType });
+			const videoUrl = URL.createObjectURL(videoBlob);
+
+			setRecordedVideo(videoUrl);
+
+			setVideoChunks([]);
+		};
+	};
+
+	return (
+		<div>
+			<h2>Video Recorder</h2>
+			<main>
+				<div className="video-controls">
+					{!permission ? (
+						<button onClick={getCameraPermission} type="button">
+							Get Camera
+						</button>
+					) : null}
+					{permission && recordingStatus === "inactive" ? (
+						<button onClick={startRecording} type="button">
+							Start Recording
+						</button>
+					) : null}
+					{recordingStatus === "recording" ? (
+						<button onClick={stopRecording} type="button">
+							Stop Recording
+						</button>
+					) : null}
+				</div>
+			</main>
+
+			<div className="video-player">
+				{!recordedVideo ? (
+					<video ref={liveVideoFeed} autoPlay className="live-player"></video>
+				) : null}
+				{recordedVideo ? (
+					<div className="recorded-player">
+						<video className="recorded" src={recordedVideo} controls></video>
+						<a download href={recordedVideo}>
+							Download Recording
+						</a>
+					</div>
+				) : null}
+			</div>
+		</div>
+	);
+};
+
+
 function App() {
   const [history, setHistory] = useState([]);
   const [fullMessage, setFullMessage] = useState(INITIAL_MESSAGE);
@@ -672,6 +805,30 @@ function App() {
     },
     [history]
   );
+  // function VideoRecording({}) {
+  //   return (
+  //     <div className="w-1/3 bg-zinc-800">
+  //       <h1 className="text-white">video section</h1>
+  //       <div className="bg-white rounded-lg shadow-md p-5 text-center">
+  //         <h1>Video Recorder</h1>
+  //         <video id="videoPreview" className="w-full max-w-xs rounded-lg mb-5" autoPlay></video>
+  //         <div className="flex justify-center gap-2.5">
+  //           <button id="startRecording">
+  //             🔴 Start Recording
+  //           </button>
+  //           <button id="stopRecording" disabled>
+  //             ⏹️ Stop Recording
+  //           </button>
+  //         </div>
+  //         {/* <a id="downloadLink" class="download-link" style="display: none">
+  //           ⬇️ Download Video
+  //         </a> */}
+  //         <h2>Transcript</h2>
+  //         <p id="transcript"></p>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   async function onMount() {
     // Warm up GPU functions.
@@ -795,6 +952,7 @@ function App() {
           </main>
           <div className="w-1/3 bg-zinc-800">
             <h1 className="text-white">video section</h1>
+            <VideoRecorder />
           </div>
         </div>
       </div>
